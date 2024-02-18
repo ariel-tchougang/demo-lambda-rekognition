393ab04405dd292479ca4a1385fbdbf6f5526c9f
Ariel Tchougang <ariel.tchougang@gmail.com>
2024-02-18 10:40:12 +0000

    Applied capital case to variables

--- demo-cleanup.sh
+++ demo-cleanup.sh
@@ -9,16 +9,16 @@ fi
 AWS_REGION=$1
 
 echo "Define the stack name"
-lambda_stack_name="demo-rekognition-lambda-stack"
+STACK_NAME="demo-rekognition-lambda-stack"
 
 echo "Retrieve the bucket name"
-bucket_name=$(aws cloudformation describe-stacks --stack-name $lambda_stack_name --region $AWS_REGION --query "Stacks[0].Outputs[?OutputKey=='DemoRekognitionBucket'].OutputValue" --output text)
+BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $AWS_REGION --query "Stacks[0].Outputs[?OutputKey=='DemoRekognitionBucket'].OutputValue" --output text)
 
 echo "Empty the S3 bucket"
-aws s3 rm s3://$bucket_name --recursive --region $AWS_REGION
+aws s3 rm s3://$BUCKET_NAME --recursive --region $AWS_REGION
 
 echo "Delete the Lambda stack"
-sam delete --stack-name $lambda_stack_name --no-prompts --region $AWS_REGION
+sam delete --stack-name $STACK_NAME --no-prompts --region $AWS_REGION
 
 echo "Cleaning up build files"
 rm -rf .aws-sam
